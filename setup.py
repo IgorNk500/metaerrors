a3120625db9b5c3f10c216b43b9539ca8e9028a8
IgorNk500 <108663923+IgorNk500@users.noreply.github.com>
2025-12-24 19:37:18 +0300

    Adding licence name into setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=['metaerrors'],
     url='https://github.com/IgorNk500/metaerrors',
-    license='',
+    license='GPL-3.0',
     author='IgorNk500',
     author_email='https://github.com/IgorNk500',
     description='Outputs errors in popups'
