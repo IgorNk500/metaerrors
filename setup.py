d7fc43032a9fbac9530f6bd577468f45ace8bfff
IgorNk500 <igornk500@outlook.com>
2025-12-24 12:09:49 +0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='metaerrors',
+    version='r1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='IgorNk500',
+    author_email='https://github.com/IgorNk500',
+    description='Outputs errors in popups'
+)
